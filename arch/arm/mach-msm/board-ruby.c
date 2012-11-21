28116465e5c2245f64d0e7d7da36fd5f22d27974
Chad Froebel <chad0989@rochester.rr.com>
2012-11-21 16:36:20 -0500

    Add ion system heap allocation
    Conflicts:
    	arch/arm/mach-msm/board-ruby.c

--- arch/arm/mach-msm/board-ruby.c
+++ arch/arm/mach-msm/board-ruby.c
@@ -138,6 +138,7 @@
 #include <mach/mdm.h>
 #include <mach/htc_util.h>
 #include <mach/board_htc.h>
+<<<<<<< HEAD:arch/arm/mach-msm/board-ruby.c
 #include <linux/akm8975.h>
 #include <linux/bma250.h>
 #include <linux/ewtzmu2.h>
@@ -198,6 +199,11 @@ static struct platform_device wl128x_device = {
 
 
 #define MSM_SHARED_RAM_PHYS 0x40000000
+=======
+#include <linux/ion.h>
+#include <mach/ion.h>
+
+>>>>>>> 6cfd31e... Add ion system heap allocation:arch/arm/mach-msm/board-vigor.c
 
 #ifdef CONFIG_PERFLOCK
 #include <mach/perflock.h>
@@ -6947,6 +6953,27 @@ static void msm_auxpcm_init(void)
 /*
  * =============== TV-out related function (BEGIN) ===============
  */
+
+#ifdef CONFIG_ION_MSM
+static struct ion_platform_data ion_pdata = {
+//	.nr = MSM_ION_HEAP_NUM,
+	.nr = 1,
+	.heaps = {
+		{
+			.id	= ION_SYSTEM_HEAP_ID,
+			.type	= ION_HEAP_TYPE_SYSTEM,
+			.name	= ION_VMALLOC_HEAP_NAME,
+		},
+	}
+};
+
+static struct platform_device ion_dev = {
+	.name = "ion-msm",
+	.id = 1,
+	.dev = { .platform_data = &ion_pdata },
+};
+#endif
+
 #ifdef CONFIG_FB_MSM_TVOUT
 static struct regulator *reg_8058_l13;
 
@@ -7182,6 +7209,9 @@ static struct platform_device *ruby_devices[] __initdata = {
 
 #ifdef CONFIG_MSM_SDIO_AL
 	&msm_device_sdio_al,
+#endif
+#ifdef CONFIG_ION_MSM
+	&ion_dev,
 #endif
 	&msm8660_device_watchdog,
 };
